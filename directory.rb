43cac31faecd6c15df2df5fe5452fff0592b83e2
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-02-10 10:17:36 +0000

    added comments to code

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+#First we print the list of students
 puts "The students of Villains Academy"
 puts "-----------------"
 puts "Dr. Hannibal Lecter"
@@ -11,6 +12,11 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+#We then print the total number of students
 print "Overall, we have "
+#It is important that print () doesn't add new line characters
 print 11
 puts " great students"
+
+#Example of using line breaks using print
+print "line1\nline2\nline3" 
